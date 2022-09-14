e6c522db157ee9dd4274f994e04a6d57abed6f1f
Cristhian Sampaio <cristhiansampaio@gmail.com>
2022-09-14 10:54:05 -0300

    Referenciando Atributos de outro Blocos - Azure Storage Account

--- azure-storage-account/storage-account.tf
+++ azure-storage-account/storage-account.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "efirst_resource_group" {
-  name     = ""
+resource "azurerm_resource_group" "first_resource_group" {
+  name     = "storage_account_resource_group"
   location = var.location
 
   tags = ""
 }
 
 resource "azurerm_storage_account" "first_storage_account" {
-  name                     = ""
-  resource_group_name      = ""
+  name                     = "cristhianstorageaccount"
+  resource_group_name      = azurerm_resource_group.first_resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
@@ -17,5 +17,5 @@ resource "azurerm_storage_account" "first_storage_account" {
 
 resource "azurerm_storage_container" "first_container" {
   name                  = ""
-  storage_account_name  = ""
+  storage_account_name  = azurerm_storage_account.first_storage_account.name
 }
